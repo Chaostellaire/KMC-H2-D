503bd1840cc4a045d28900f597186f147e9cb9e6
Chaostellaire <harmonie.lebrun1@gmail.com>
2024-10-16 16:29:06 +0200

    time to see computation of traj

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ Main.py : file to launch to execute the program, will countain basic function an
 Chaostellaire : Harmonie, 
 
 TO DO:
-- (Object branch) turn Paramets into a class, easy feed
 - turn this into CLI for easy modification of Parameters values
 """
 import kinmontecarlo as KMC;
@@ -13,13 +12,14 @@ import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import savings as savs
+from time import time
 
 
 Parameters = {
     #~~ OVERALL SYSTEM PROPERTIES ~~
     
     "GAMMA" : 1, #float, dimensionnal value for GAMMAs. [Hz]
-    "GAMMA1_SHARE" : 0.5, #float, GAMMA1/GAMMA, share of GAMMA1, GAMMA2_share = 1-GAMMA1/GAMMA. To get back to dimensionnal values we just need to multiply by GAMMA.
+    "GAMMA1_SHARE" : 0.25, #float, GAMMA1/GAMMA, share of GAMMA1, GAMMA2_share = 1-GAMMA1/GAMMA. To get back to dimensionnal values we just need to multiply by GAMMA.
     "lattice parameter" : 3., #float, refers to a, usefull only to get dimensionnal numbers back.
     
     #~~ SIMULATION VARIABLES ~~
@@ -42,12 +42,12 @@ Parameters = {
 }
 
 
+start = time()
 
 
 
-
-KMC.init_parameters(Parameters)
 L = KMC.trajectory(Parameters)
+print("computation of trajectory done at {}".format(time() - start))
 if Parameters["table save flag"] :
     savs.save2file(Parameters,L)
 if Parameters["visu"] :
