72c01d0f926f4aac2be881351a3a4c63d129e6ef
Chaostellaire <harmonie.lebrun1@gmail.com>
2024-10-23 15:09:17 +0200

    solved msqrt issue

--- kinmontecarlo.py
+++ kinmontecarlo.py
@@ -92,11 +92,14 @@ def computeDiffusion_normalized(trajectory_vector:np.ndarray, Parameters: dict) 
     Computes a mean value of D/Gamma we need to recover : D = 1/4 (Gamma1 d1² + Gamma2 d2²)
     """
     D_storage = 0.0
-    for k_steps in range(1,Parameters["steps"]+1):
-        D_storage += MQV(trajectory_vector, k_steps) / (4*k_steps)
-    return D_storage / (Parameters["steps"])
-
-
+    # We take only a few points to faster the computation speed, bc we know that we should get a linear asymptot
+    # We also exclude the first 20%, because at low time scales the hydrogen didn't mooved that much across the lattice
+    # We exclude the last 20% because they have too less point to compute a statifying mean
+    n = trajectory_vector.shape[0]
+    taken_steps = np.linspace(0.2*n,0.8*n,100, dtype = int)
+    for k_step in taken_steps:
+        D_storage += MQV(trajectory_vector, k_step) / (4*k_step)
+    return D_storage / 100
 
 
 
--- main.py
+++ main.py
@@ -18,30 +18,28 @@ import numpy as np
 
 Parameters = {
 
-    "Model" : 2,
+    "Model" : 1,
 
     #~~ OVERALL SYSTEM PROPERTIES ~~
     
     "GAMMA" : 1, #float, dimensionnal value for GAMMAs. [Hz]
     "GAMMA1_SHARE" : 0.25, #float, GAMMA1/GAMMA, share of GAMMA1, GAMMA2_share = 1-GAMMA1/GAMMA. To get back to dimensionnal values we just need to multiply by GAMMA.
-    "lattice parameter" : 3., #float, refers to a, usefull only to get dimensionnal numbers back.
-    
+
     "a" : 1,
     "b" : 0.3,
 
     #~~ SIMULATION VARIABLES ~~
-    "steps" : 100, #int, step number for simulation, output is size steps+1 (storing starting (0,0) position)
+    "load a table" : False, #bool, need to select correct parameter to load correct table
+    "steps" : 1000000, #int, step number for simulation, output is size steps+1 (storing starting (0,0) position)
     
     #~~ SAVING PROPERTIES ~~#
     "table save flag" : True,
-    "saving type" : ".npy" , #str, gives the format of saving of the tables. .npy is recommanded #### ".npy",  ".dat", ".txt"
-    
-    "steps" : 300, #int, step number for simulation, output is size steps+1 (storing starting (0,0) position)
+    "saving type" : "npy" , #str, gives the format of saving of the tables. npy is recommanded #### "npy",  "dat", "txt"
 
     #~~ VISUALIZATION ~~
     "visu" : True, #bool 
     "fps"  : 12, #int
-    "D_computation" : True, #bool
+    "D_computation" : False, #bool
 }
 
 
@@ -49,31 +47,36 @@ start = time()
 
 
 
-L = KMC.trajectory(Parameters)
-print("computation of trajectory done at {}".format(time() - start))
+if Parameters["load a table"] :
+    L = np.load("GAMMA1_SHARE_{}/step_{}.npy".format(Parameters["GAMMA1_SHARE"], Parameters["steps"]))
+else:
+    if Parameters["Model"] == 1:
+        L = KMC.trajectory_1(Parameters)
+        print("computation of trajectory done at {}".format(time() - start))
+    if Parameters["Model"] == 2:  
+        L = np.array([sublist[0] for sublist in KMC.trajectory_2(Parameters)])
+        compute_time = time() - start
+        print("computation of trajectory done at {}".format(compute_time))
 
-if Parameters["Model"] == 1:
-    L = KMC.trajectory_1(Parameters)
-if Parameters["Model"] == 2:  
-    L = np.array([sublist[0] for sublist in KMC.trajectory_2(Parameters)])
-
-if Parameters["table save flag"] :
-    savs.save2file(Parameters,L)
+    if Parameters["table save flag"] :
+        savs.save2file(Parameters,L)
+        print("time to save : {}s".format(time()-compute_time))
 
 if Parameters["visu"] :
-    visu.animate_simulation(L, Parameters)
-
+    #visu.animate_simulation(L, Parameters)
+    visu.Diffusion_plot(Parameters,L)
 #check for D vallidity
 
-visu.Diffusion_plot(Parameters,L)
 
 
-"""
-D_computed = KMC.computeDiffusion_normalized(L,Parameters)
-D_computed = KMC.MQV(L,20) / 80
-print("Ds are : ")
-print("real value = {}  ||| computed value = {}".format(D_true, D_computed))
-"""
+if Parameters["D_computation"] : 
+    D_computed = KMC.computeDiffusion_normalized(L,Parameters)
+    D_true = 1/4 * (Parameters["GAMMA1_SHARE"] + 2 * (1-Parameters["GAMMA1_SHARE"]))
+    print("================================")
+    print("Ds are : ")
+    print("real value = {}  ||| computed value = {}".format(D_true, D_computed))
+    print("Error :      {:.1%}".format(np.abs(D_true-D_computed)/D_true))
+
 
 
 
