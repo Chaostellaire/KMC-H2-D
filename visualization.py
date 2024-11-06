e47231bd1631661f127ab14745f71501bf4310a6
Chaostellaire <harmonie.lebrun1@gmail.com>
2024-11-06 15:19:37 +0100

    schöner label

--- visualization.py
+++ visualization.py
@@ -62,7 +62,8 @@ def Diffusion_plot(Parameters : dict, trajectory_vector : np.ndarray) -> None :
         D_true = 1/4*(Parameters["GAMMA1_SHARE"]*(1-Parameters["GAMMA1_SHARE"])) * (Parameters['a']-2*Parameters['b'])**2
         
     #O(n²)
-    n = trajectory_vector.shape[0]
+    #We only care about the first percents of the trajectory
+    n = trajectory_vector.shape[0]*0.02
     if Parameters["load mqv"] and Parameters["load traj"]:
         if Parameters["custom load"]:
             MQV_compute= np.load(f"GAMMA1_SHARE_{Parameters['GAMMA1_SHARE']}/{Parameters['load path']}_mqv.{Parameters['saving type']}")[0]
@@ -72,10 +73,11 @@ def Diffusion_plot(Parameters : dict, trajectory_vector : np.ndarray) -> None :
             taken_steps = np.load("GAMMA1_SHARE_{}/model{}_step_{}_mqv.npy".format(Parameters["GAMMA1_SHARE"],Parameters['Model'], Parameters["steps"]))[1]
     else :
         #taken_steps = np.linspace(100, n+1, 20, dtype = int)
-        taken_steps = np.linspace(0.2*n,0.8*n,20, dtype = int)
+        taken_steps = np.linspace(n/20,n,20, dtype = int)
         MQV_compute = np.array([MQV(trajectory_vector, k) for k in taken_steps])
         savings.save2file(Parameters,np.stack((MQV_compute,taken_steps), axis = 0),"mqv")
            #to print we multiply 4Dt where t is adimensional so : t = step_number
+    
     MQV_true_table = [4*D_true*1,4*D_true*n]
     D_eff1 = 1/4*(Parameters["GAMMA1_SHARE"]*(1-Parameters["GAMMA1_SHARE"])) * (Parameters['a']**2-2*Parameters['a']*Parameters['b']+Parameters['b']**2)
     MQV_eff1_table = [4*D_eff1*1,4*D_eff1*n]
@@ -87,19 +89,22 @@ def Diffusion_plot(Parameters : dict, trajectory_vector : np.ndarray) -> None :
     
     fig, ax = plt.subplots(figsize = (16,9))
     ax.set_xlim(1,n)
-    ax.plot([1,n], MQV_true_table, label ="Real <[x(t0-t)-x(t0)]²> value", color = "blue", 
+    ax.plot([1,n], MQV_true_table, label ="<[x(t0-t)-x(t0)]²> = Gamma1*Gamma2/Gamma*(a-2b)²t", color = "blue", 
             linestyle = "solid")
     #
     ax.plot(taken_steps,MQV_compute, label = "Computed <[x(t0-t)-x(t0)]²> value", color = 'red', 
             linestyle = "dotted", marker = "^")
-    ax.plot([1,n],MQV_eff1_table, label = "Effective", color = 'forestgreen')
-    ax.plot([1,n],MQV_eff2_table, label = "Effective", color = 'black')
+    ax.plot([1,n],MQV_eff1_table, label = "<[x(t0-t)-x(t0)]²> = Gamma1*Gamma2/Gamma*(a²+b²-2ab)t", color = 'forestgreen')
+    ax.plot([1,n],MQV_eff2_table, label = "<[x(t0-t)-x(t0)]²> = Gamma1*Gamma2/(2Gamma1+Gamma2)*a²t", color = 'black')
 
     #
     ax.legend()
-    if ["custom load"]:
+    ax.set_xlabel("steps (or time steps kt = t) ")
+    ax.set_ylabel("<[x(t0-t)-x(t0)]²>")
+    ax.set_title(f"Diffusion coefficient D, with respect to time on a {Parameters["steps"]} trajectory")
+    if Parameters["custom load"]:
         plt.savefig(f"GAMMA1_SHARE_{Parameters['GAMMA1_SHARE']}/{Parameters['load path']}")
     else :
         plt.savefig(f"GAMMA1_SHARE_{Parameters['GAMMA1_SHARE']}/model{Parameters['Model']}_step_{Parameters['steps']}")
-    plt.show()
+    
     
